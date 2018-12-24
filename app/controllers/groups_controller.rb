7be4941abec4b786eec4e1617ba2207eb7279567
hosidalt <itie0112@yahoo.co.jp>
2018-12-24 15:07:12 +0900

    Edit_function7
    グループ作成機能の実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,26 +1,23 @@
 class GroupsController < ApplicationController
+  def index
+  end
 
   def new
+    @group = Group.new
+    @group.users << current_user
   end
 
   def create
-  end
-
-  def edit
-  end
-
-  def update
-      if current_user.update(user_params)
-      redirect_to root_path
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
     else
-      render :edit
+      render :new
+    end
   end
-
-end
 
   private
-
-  def user_params
-    params.require(:user).permit(:name, :email)
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => [] })
   end
 end
