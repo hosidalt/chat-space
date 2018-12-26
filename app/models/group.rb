2e73657131c221be9d52013ce604c4d1cc42f767
hosidalt <itie0112@yahoo.co.jp>
2018-12-26 19:48:38 +0900

    edit_post1
    Messageモデルを作成

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,9 @@
 class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
+  class Message < ApplicationRecord
+  belongs_to :group
+  belongs_to :user
+  has_many :messages
   validates :name, presence: true
 end
