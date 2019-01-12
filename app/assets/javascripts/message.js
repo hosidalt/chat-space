c147bf4defc99d439fa3ca6fd655ee96f90c7e31
hosidalt <itie0112@yahoo.co.jp>
2019-01-12 12:21:12 +0900

    desynchronisation2
    ・Ajaxを使用して、messages#createが動くようにする
    ・respond_toを使用してHTMLとJSONの処理を分け

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,16 @@
 $(function(){
   $('#new_message').on('submit',function(e){
     e.preventDefault();
-    console.log(this)
       var formData = new formData(this);
 
+      var url = $(this).attr('action')
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+  })
+})
