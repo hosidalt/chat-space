0b4f8228069c144d43fc0b96c4bb2b7feb06f875
hosidalt <itie0112@yahoo.co.jp>
2019-01-13 14:42:56 +0900

    Resynchronization4
    返ってきたJsonを受け取るdoneメッソドと表示するHTMLの作成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,16 +1,43 @@
 $(function(){
+  function buildHTML(message){
+   const addImage = (message.image)?`<img = "${message.image}">`:''
+   const html = `<div class="chat__body__list">
+                 <div class= "chat__body__list__message.clearfix">
+                   <div class= "chat__body__list__message__name">
+                     ${message.user_name}
+                   </div>
+                   <div class= "chat__body__list__message__time">
+                     ${ message.created_at }
+                   </div>
+                 </div>
+                 <div class= "chat__body__list__message__body">
+                   ${message.content}
+                 <img src="${addImage}">
+                 </div>
+               </div>`
+
+    return html;
+  }
   $('#new_message').on('submit',function(e){
     e.preventDefault();
-      var formData = new formData(this);
+      var formData = new FormData(this);
 
-      var url = $(this).attr('action')
+      var url = $(this).attr('action');
       $.ajax({
-        url: url,
         type: "POST",
+        url: url,
         data: formData,
         dataType: 'json',
         processData: false,
         contentType: false
       })
-  })
-})
+      .done(function(data){
+        var html = buildHTML(data);
+        $('.chat__body__list').append(html)
+        $('.submit').attr("disabled",false)
+        $('.chat__footer__form-body__form-message').val('')
+        function getScrollTop(){
+          $('.chat__body')
+        }
+
+
