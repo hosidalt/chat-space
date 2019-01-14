1442c74048ca24789c454b859b066ba8d434e2e6
hosidalt <itie0112@yahoo.co.jp>
2019-01-14 17:32:44 +0900

    Incremental-Search3
    ・HTMLをビュー上に追加
    ・非同期通信の結果を受け取るdoneとエラー時の処理を編集

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,12 @@
 $(function(){
 
-
+function appendUser(user){
+  var html = `<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                  <a class= "user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id = ${user.id} data-user-name = ${user.name} > 追加
+              </div>`
+  return html;
+  }
 
   $('#user-search-field').on('keyup',function(){
     var input = $('#user-search-field').val();
@@ -12,7 +18,18 @@ $(function(){
       dataType: 'json'
     })
 
-
+    .done(function(data){
+      if(data.length !== 0){
+        data.forEach(function(user){
+          var html = appendUser(user);
+          $('.user-search-result').append(html)
+        });
+      }
+      // $('#user-search-field')[0].reset();
+    })
+    .fail(function(){
+      alert('ユーザー検索は失敗しました');
+    })
 
 
 
