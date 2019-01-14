ab8c45c5fa9f2794add1ef9500e55ee5bdf168ac
hosidalt <itie0112@yahoo.co.jp>
2019-01-14 17:25:38 +0900

    Incremental-Search2
    イベント時に非同期通信できるように編集

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,20 @@
 $(function(){
-  $('.chat-group-form__input').on('keyup',function(){
-    var input = $('.chat-group-form__input').val();
-    console.log(input);
-  })
-})
+
+
+
+  $('#user-search-field').on('keyup',function(){
+    var input = $('#user-search-field').val();
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+
+
+
+
+  });
+});
