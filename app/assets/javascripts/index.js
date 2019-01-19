fd9c25437de1bee66aad716411f859ae6a7d4f5c
hosidalt <itie0112@yahoo.co.jp>
2019-01-19 18:22:55 +0900

    Incremental-Search5
    ・追加されるビューの編集
    ・追加ボタンを押した後のアクション編集
    ・削除ボタンを押した後のアクションを編集

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -6,10 +6,20 @@ function appendUser(user){
                   <a class= "user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id = ${user.id} data-user-name = ${user.name} > 追加
               </div>`
   return html;
+  };
+function appendUserName(name ,id){
+  var html =`<div class='chat-group-user clearfix js-chat-member'>
+               <input name='group[user_ids][]' type='hidden' value= ${id}>
+               <p class='chat-group-user__name'>${name}</p>
+               <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+             </div>`
+  return html;
   }
 
+  // インクリメンタルサーチ
   $('#user-search-field').on('keyup',function(){
     var input = $('#user-search-field').val();
+    $('.user-search-result').empty();
 
     $.ajax({
       type: 'GET',
@@ -19,6 +29,7 @@ function appendUser(user){
     })
 
     .done(function(data){
+
       if(data.length !== 0){
         data.forEach(function(user){
           var html = appendUser(user);
@@ -30,15 +41,24 @@ function appendUser(user){
     .fail(function(){
       alert('ユーザー検索は失敗しました');
     })
-
-function buildHTML(name){
-  var html =``
-}
-
-  $(document).on('click','.user-search-add',function(){
-    buildHTML()
-  })
-
-
   });
+
+
+  // 追加ボタン
+  $('.user-search-result').on('click','.user-search-add',function(){
+    var name = $(this).data("user-name");
+    var id = $(this).data("user-id");
+    var html = appendUserName(name,id);
+    $('#user-name').append(html);
+    // 検索結果から削除
+    $(this).parent().remove();
+  });
+
+  $('#user-name').on('click','.user-search-remove',function(){
+    $(this).parent().remove();
+  });
+
+
 });
+
+
