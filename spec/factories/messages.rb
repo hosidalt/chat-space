8cd5979261db2cbbf7f5b4d026d9c7301509fa04
hosidalt <itie0112@yahoo.co.jp>
2019-01-26 20:26:32 +0900

    Unicorn

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :message do
     content Faker::Lorem.sentence
-    image File.open("#{Rails.root}/public/images/DSC_4330.jpg")
+    image File.open("#{Rails.root}/public/images/DSC_4330.JPG")
     user
     group
   end
