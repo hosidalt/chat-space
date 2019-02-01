63a7d7c02754fa08d61b1d9c05c186a01da1584f
hosidalt <itie0112@yahoo.co.jp>
2019-02-01 20:26:03 +0900

    edit imageuploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,7 +13,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   if Rails.env.development? || Rails.env.test?
   #     storage :fog
   #   else
-  #     storage :fog
+  storage :fog
   #   end
   # end
 
